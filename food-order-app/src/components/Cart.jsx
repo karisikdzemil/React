6db34ab51ff18d2299110758b3d06eadf6d9c52d
dzemil karisik <karisikdzemil@gmail.com>
2025-03-17 21:52:59 +0100

    creating CartItem

--- food-order-app/src/components/Cart.jsx
+++ food-order-app/src/components/Cart.jsx
@@ -1,9 +1,10 @@
 import { useContext } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "../store/CartContext";
-import { currencyFormatter } from '../util/formatting.js';
-import Button from './UI/Button.jsx';
+import { currencyFormatter } from "../util/formatting.js";
+import Button from "./UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -14,23 +15,30 @@ export default function Cart() {
     0
   );
 
-  function handleCloseCart(){
+  function handleCloseCart() {
     userProgressCtx.hideCart();
   }
 
   return (
-    <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly onClick={handleCloseCart}>Close</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
         <Button onClick={handleCloseCart}>Go to Checkout</Button>
       </p>
     </Modal>
