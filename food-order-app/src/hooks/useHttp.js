b401aeafafa867477cf2f9dcaf5a21c831d9e2e1
dzemil karisik <karisikdzemil@gmail.com>
2025-03-22 22:10:11 +0100

    finishing touches

--- food-order-app/src/hooks/useHttp.js
+++ food-order-app/src/hooks/useHttp.js
@@ -19,10 +19,14 @@ export default function useHttp(url, config, initialData) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const sendRequest = useCallback(async function sendRequest() {
+  function clearData(){
+    setData(initialData);
+  }
+
+  const sendRequest = useCallback(async function sendRequest(data) {
     setIsLoading(true);
     try {
-      const resData = await sendHttpRequest(url, config);
+      const resData = await sendHttpRequest(url, {...config, body: data});
       setData(resData);
     } catch (error) {
       setError(error.message || "Something went wrong!");
@@ -36,10 +40,11 @@ useEffect(() => {
   }
 }, [sendRequest, config])
 
-  return {
+  return {      
     data,
     isLoading,
     error,
-    sendRequest
+    sendRequest,
+    clearData
   };
 }
